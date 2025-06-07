d4971ef0417ed1cfdb4c26d297c27b8e3d6f98f1
leonardo dantas <leonardo1317@yahoo.com.br>
2025-06-07 07:17:40 -0300

    feat: Criando mappers

---
+++ src/main/java/com/netshoes/wishlist/infra/mappers/WishlistMapper.java
@@ -0,0 +1,13 @@
+package com.netshoes.wishlist.infra.mappers;
+
+import com.netshoes.wishlist.domain.Wishlist;
+import com.netshoes.wishlist.infra.database.documents.WishlistDocument;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface WishlistMapper {
+
+    WishlistDocument toDocument(final Wishlist wishlist);
+
+    Wishlist toDomain(final WishlistDocument document);
+}
