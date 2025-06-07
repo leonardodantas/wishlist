53f6552e936217d33cd883c767ad211af5c263ed
leonardo dantas <leonardo1317@yahoo.com.br>
2025-06-07 07:18:32 -0300

    feat: Adicionando repository junto ao MongoRepository

---
+++ src/main/java/com/netshoes/wishlist/infra/database/documents/repositories/WishListMongoRepository.java
@@ -0,0 +1,11 @@
+package com.netshoes.wishlist.infra.database.documents.repositories;
+
+import com.netshoes.wishlist.infra.database.documents.WishlistDocument;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface WishListMongoRepository extends MongoRepository<WishlistDocument, String> {
+
+    Optional<WishlistDocument> findByCustomerId(final String customerId);
+}
---
+++ src/main/java/com/netshoes/wishlist/infra/database/documents/repositories/WishListRepository.java
@@ -0,0 +1,30 @@
+package com.netshoes.wishlist.infra.database.documents.repositories;
+
+import com.netshoes.wishlist.app.repositories.IWishlistRepository;
+import com.netshoes.wishlist.domain.Wishlist;
+import com.netshoes.wishlist.infra.database.documents.WishlistDocument;
+import com.netshoes.wishlist.infra.mappers.WishlistMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class WishListRepository implements IWishlistRepository {
+
+    private final WishlistMapper wishlistMapper;
+    private final WishListMongoRepository wishListMongoRepository;
+
+    @Override
+    public Optional<Wishlist> findByCustomerId(final String customerId) {
+        return wishListMongoRepository.findByCustomerId(customerId)
+                .map(wishlistMapper::toDomain);
+    }
+
+    @Override
+    public void save(final Wishlist wishlist) {
+        WishlistDocument document = wishlistMapper.toDocument(wishlist);
+        wishListMongoRepository.save(document);
+    }
+}
