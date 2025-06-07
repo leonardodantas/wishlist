e7d0bd1fb40cc61793e3345fd4e3bc4987d42f13
leonardo dantas <leonardo1317@yahoo.com.br>
2025-06-07 07:04:38 -0300

    feat: Criando exceções que representem violações nos dominios

---
+++ src/main/java/com/netshoes/wishlist/domain/exceptions/WishlistLimitReachedException.java
@@ -0,0 +1,8 @@
+package com.netshoes.wishlist.domain.exceptions;
+
+public class WishlistLimitReachedException extends RuntimeException {
+
+    public WishlistLimitReachedException(final String customerId) {
+        super("Wishlist limit reached for customer with ID " + customerId + ".");
+    }
+}
